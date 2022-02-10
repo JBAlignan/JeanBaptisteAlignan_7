73d1daa399e84e3173335546421e6c5d70332f22
unknown <jeanbaptiste.alignan@hotmail.fr>
2022-02-10 16:11:24 +0100

    Ajout du début du frontend; suppression de node_module pour ne pas le uplaod

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<!--JS-->
+<script>
+
+</script>
+
+
+<!--HTML-->
+<template>
+	<div id="app">
+		<nav>
+			<a href="/"><img src="./assets/icon-above-font-resized.png"></a>
+		</nav>
+	</div>
+</template>
+
+
+
+<!--SCSS-->
+<style lang="scss">
+  #app {
+    display: flex;
+    width: 100%;
+    nav {
+      width: 100%;
+      border: 1px solid black;
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+             a {
+        border: 1px solid black;
+      }
+    } 
+  }  
+ 
+  
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+	name: 'Header'
+}
+</script>
+
+<template>
+	<a href="/">Accueil</a> 
+</template>
+
+<style>
+	a {
+		text-decoration: none;
+	}
+</style>
